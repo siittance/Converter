ce42a9604147d2c61fce90a3712cab6c02e3efa3
siittance <isip_yu.v.ivanova>
2023-11-07 23:24:24 +0300

    Добавьте файлы проекта.

---
+++ Converter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Text;
+using static System.Console;
+
+namespace Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+            InputEncoding = Encoding.UTF8;
+            OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
