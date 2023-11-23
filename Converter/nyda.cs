5fcbbdc2db7e9fabb4c6f2a3ccea7e860157de1a
siittance <144900566+siittance@users.noreply.github.com>
2023-11-23 17:30:05 +0300

    nyda.cs

---
+++ Converter/nyda.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//ну ваще тут можно потыкать и будет не только ТЕКСТ, ЦИФРА, ЦИФРА читаться (типы данных поменять)
+namespace Converter
+{
+    internal class nyda
+    {
+        public string Chtoto;
+        public int Cifra1;
+        public int Cifra2;
+        public nyda(string chtoto, int cifra1, int cifra2)
+        {
+            Chtoto = chtoto;
+            Cifra1 = cifra1;
+            Cifra2 = cifra2;
+        }
+    }
+}
