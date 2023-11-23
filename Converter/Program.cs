618d0739d35cdbd82db17130cb39881f5fd29c1f
siittance <144900566+siittance@users.noreply.github.com>
2023-11-23 17:27:06 +0300

    Program.cs

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -1,6 +1,11 @@
-﻿using System.Globalization;
+using System.Globalization;
 using System.Text;
-using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
 
 namespace Converter
 {
@@ -8,10 +13,17 @@ namespace Converter
     {
         static void Main(string[] args)
         {
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
-            InputEncoding = Encoding.UTF8;
-            OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("Hello, World!");
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введите путь до файла + позывной, который вы хотите открыть");
+            Console.WriteLine("=====================================================================");
+            List<nyda> kakoitotxt = Nechinalka.Read(Console.ReadLine());
+            kakoitotxt = Chinalka.Change(kakoitotxt);
+            Console.Clear();
+            Console.WriteLine("Введите путь до файла + позывной, куда вы хотите сохранить текст");
+            Console.WriteLine("=====================================================================");
+            Nechinalka.Save(Console.ReadLine(), kakoitotxt);
         }
     }
 }
